77b42c21014a70e40f6c09db076b05bc9a8d6f7d
VladimirJaraica <vladojarabica@gmail.com>
2016-04-07 07:48:16 +0000

    Specify port and ip

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ import path from 'path'
 import config from './webpack.config.js'
 
 export default () => {
-	let app = express();
+	let app = express()
+	
+	let port = process.env.PORT || 3000
+	let ip = process.env.IP || 'localhost'
 
-	app.use(express.static('./public'));
+	app.use(express.static('./public'))
 
 	app.use('/', (req, res) => {
-	    res.sendFile(path.resolve('public/index.html'));
-	});
+	    res.sendFile(path.resolve('public/index.html'))
+	})
 
-	let port = 3000;
 
-	app.listen(port, error => {
+	app.listen(port, ip, error => {
 	  if (error) throw error;
-	  console.log("Express server listening on port", port);
-	});
-
+	  console.log("Express server listening on port", port)
+	})
+	
 }
